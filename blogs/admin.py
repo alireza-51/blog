11a21dfeb1ae35964219283e1c1d6e820e65b919
alireza <alirezazarei51@gmail.com>
2024-02-10 17:49:26 +0330

    add blogs to admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Post, Tag, Comment
 
-# Register your models here.
+admin.site.register([Tag, Comment])
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    model = Post
+
+    list_display = (
+        "id",
+        "title",
+        "slug",
+    )
+    # list_filter = (
+    #     "published",
+    #     "publish_date",
+    # )
+    list_editable = (
+        "title",
+        "slug",
+    )
+    search_fields = (
+        "title",
+        "slug",
+        "content",
+    )
+    prepopulated_fields = {
+        "slug": (
+            "title",
+        )
+    }
+    date_hierarchy = "updated"
+    save_on_top = True
