5af4e1fddfa079b927dffe76983a5b53ec919675
alireza <alirezazarei51@gmail.com>
2024-02-10 17:36:00 +0330

    create blogs db models

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self) -> str:
+        return f'{self.name}'
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE, related_name='posts',
+        null=False,
+        blank=False
+    )
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=150, unique=True)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        to=Post,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='comments'
+    )
+    user = models.ForeignKey(
+        to=User,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='comments'
+    )
+    caption = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    parent = models.ForeignKey(
+        'self',
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+        related_name='replies'
+    )
--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Apps
+    'blogs.apps.BlogsConfig',
+
+    # Built-in Apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,13 +77,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",  # set in docker-compose.yml
+        "PORT": 5432,  # default postgres port
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
