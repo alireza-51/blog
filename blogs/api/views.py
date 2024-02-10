0ff138cd7cba25b67c09ce56ed756e16399b75ad
alireza <alirezazarei51@gmail.com>
2024-02-10 22:39:19 +0330

    change blogs permission to custom permissions

--- blogs/api/views.py
+++ blogs/api/views.py
@@ -1,19 +1,22 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from blogs.models import Post, Tag, Comment
 
 from .serializers import PostSerializer, CommentSerializer, TagSerializer
-
+from utils.custom_permissions import IsAuthorOrCreateOrReadOnly
 
 class PostViewset(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
+    permission_classes = [IsAuthorOrCreateOrReadOnly]
 
 
 class CommentViewset(viewsets.ModelViewSet):
     serializer_class = CommentSerializer
     queryset = Comment.objects.all()
+    permission_classes = [IsAuthorOrCreateOrReadOnly]
 
 
 class TagViewset(viewsets.ModelViewSet):
     serializer_class = TagSerializer
     queryset = Tag.objects.all()
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
