2aa240231c34ef3a5f8e53f85824b654391d8870
alireza <alirezazarei51@gmail.com>
2024-02-10 17:57:34 +0330

    create user profile model

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+        null=False,
+        blank=False,
+        related_name='profile'
+    )
+    bio = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.user.get_username()
