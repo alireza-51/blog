a9e0b30a53bcbb8bc0829e6ccb97815f3748069b
alireza <alirezazarei51@gmail.com>
2024-02-10 21:32:10 +0330

    add permision to user apis

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from django.contrib.auth import get_user_model
 from profiles.models import Profile
 from .serializers import ProfileSerializer, UserSerializer
@@ -9,8 +10,10 @@ User = get_user_model()
 class ProfileViewset(viewsets.ModelViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
 
 
 class UsersViewset(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+    permission_classes = [permissions.IsAuthenticated]
