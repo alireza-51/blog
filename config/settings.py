fcdc719f8f975dd37d06ec8458d876caa95281f0
alireza <alirezazarei51@gmail.com>
2024-02-11 14:43:19 +0330

    add pagination to drf

--- config/settings.py
+++ config/settings.py
@@ -149,7 +149,9 @@ REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
 }
 
 # drf-spectacular settings
